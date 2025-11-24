3acacbce64853c0424abb5498613376abd404b26
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-11-24 02:00:50 +0200

    update TARGET_KERNEL_DIR for laguna devices

--- config/mk/google_devices/device/frankel/device.mk
+++ config/mk/google_devices/device/frankel/device.mk
@@ -1,6 +1,5 @@
 $(call inherit-product, vendor/adevtool/config/mk/google_devices/platform/laguna/product-common.mk)
 
-TARGET_KERNEL_DIR = device/google/laguna-kernels/6.6/grapheneos
-#TARGET_BOARD_KERNEL_HEADERS ?= device/google/laguna-kernels/6.6/grapheneos/kernel-headers
+TARGET_KERNEL_DIR := device/google/laguna-kernels/6.6/grapheneos/muzel
 
 include vendor/adevtool/config/mk/google_devices/platform/laguna/device.mk
--- config/mk/google_devices/device/rango/device.mk
+++ config/mk/google_devices/device/rango/device.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, vendor/adevtool/config/mk/google_devices/platform/laguna/product-common.mk)
 
-TARGET_KERNEL_DIR = device/google/laguna-kernels/6.6/grapheneos
+TARGET_KERNEL_DIR := device/google/laguna-kernels/6.6/grapheneos/rango
 
 include vendor/adevtool/config/mk/google_devices/platform/laguna/device.mk
