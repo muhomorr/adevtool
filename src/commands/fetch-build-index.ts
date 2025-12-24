30ff9d290adb9d6be4bfa7db3afc05fc1e2073cd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-12-24 17:52:38 +0200

    add support for fetching canary build index from a Markdown file

--- src/commands/fetch-build-index.ts
+++ src/commands/fetch-build-index.ts
@@ -1,8 +1,17 @@
 import { Command, Flags } from '@oclif/core'
 import { YAMLMap } from 'yaml/types'
 
-import { DEVICE_CONFIGS_FLAG, loadDeviceConfigs } from '../config/device'
-import { fetchBetaBuildIndex, fetchBuildIndex } from '../images/build-index'
+import assert from 'assert'
+import YAML from 'yaml'
+import {
+  DEVICE_CONFIGS_FLAG,
+  DeviceBuildId,
+  DeviceConfig,
+  loadDeviceConfigs,
+  makeDeviceBuildId,
+} from '../config/device'
+import { BuildIndex, BuildProps, fetchBetaBuildIndex, fetchBuildIndex, ImageType } from '../images/build-index'
+import { readFile } from '../util/fs'
 import { yamlStringifyNoFold } from '../util/yaml'
 
 export default class FetchBuildIndex extends Command {
@@ -11,8 +20,10 @@ export default class FetchBuildIndex extends Command {
   static flags = {
     ...DEVICE_CONFIGS_FLAG,
     beta: Flags.string({
-      char: 'b',
-      description: 'Fetch index of beta builds for the specified major OS version (e.g. -b 14)',
+      description: 'Fetch index of beta builds for the specified major OS version (e.g. --beta 14)',
+    }),
+    canary: Flags.string({
+      description: 'Fetch index of canary builds from a Markdown file',
     }),
   }
 
@@ -21,13 +32,68 @@ export default class FetchBuildIndex extends Command {
     let devices = await loadDeviceConfigs(flags.devices)
 
     let index: YAMLMap
-    if (flags.beta === undefined) {
-      index = await fetchBuildIndex(devices)
-    } else {
+    if (flags.beta !== undefined) {
       index = await fetchBetaBuildIndex(devices, flags.beta)
+    } else if (flags.canary !== undefined) {
+      index = await parseCanaryBuildIndex(devices, flags.canary)
+    } else {
+      index = await fetchBuildIndex(devices)
     }
 
     let yaml = yamlStringifyNoFold(index)
     this.log(yaml)
   }
+}
+
+async function parseCanaryBuildIndex(deviceConfigs: DeviceConfig[], srcFilePath: string) {
+  let deviceNames = new Set<string>()
+  for (let config of deviceConfigs) {
+    deviceNames.add(config.device.name)
+  }
+
+  let buildIndex: BuildIndex = new Map<DeviceBuildId, BuildProps>()
+
+  let builds = (await readFile(srcFilePath)).split('# Android Canary ')
+  for (let build of builds) {
+    if (build.length === 0) {
+      continue
+    }
+    let lines = build.split('\n')
+    let buildIdLine = lines[0]
+    let buildIdStart = buildIdLine.indexOf('ZP1')
+    assert(buildIdStart > 0, buildIdLine)
+    let buildIdEnd = buildIdLine.indexOf(' ', buildIdStart)
+    assert(buildIdEnd > buildIdStart, buildIdLine)
+    let buildId = buildIdLine.substring(buildIdStart, buildIdEnd)
+    for (let line of lines.slice(1)) {
+      let parts = line.split(' | ')
+      if (parts.length < 4) {
+        continue
+      }
+      let devicePart = parts[0]
+      let deviceStart = devicePart.lastIndexOf('(')
+      if (deviceStart < 2) {
+        continue
+      }
+      deviceStart += 1
+      let deviceEnd = devicePart.lastIndexOf(')')
+      assert(deviceEnd > deviceStart, line)
+      let device = devicePart.substring(deviceStart, deviceEnd)
+      if (!deviceNames.has(device)) {
+        continue
+      }
+      let dlPart = parts[2]
+      let dlPrefix = '[Link]('
+      assert(dlPart.startsWith(dlPrefix), line)
+      assert(dlPart.endsWith(')'), line)
+      let dlLink = dlPart.slice(dlPrefix.length, -1)
+      let sha256 = parts[3]
+      assert(sha256.endsWith(' |'), line)
+      sha256 = sha256.slice(0, -2)
+      let props = new Map<string, string>()
+      props.set(ImageType.Factory, sha256 + ' ' + dlLink)
+      buildIndex.set(makeDeviceBuildId(device, buildId), props)
+    }
+  }
+  return YAML.createNode(buildIndex) as YAMLMap
 }
