c55d37020b4899a7dcd81cd5ffd2f0adeabcbb82
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-08-22 19:40:02 +0000

    fix TARGET_KERNEL_DIR declaration for 10th gen Pixels
    Don't redefine TARGET_KERNEL_DIR, that breaks the USE_STOCK_KERNEL=true option.

--- config/mk/google_devices/device/stallion/device.mk
+++ config/mk/google_devices/device/stallion/device.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, vendor/adevtool/config/mk/google_devices/platform/zumapro/product-common.mk)
 
-TARGET_KERNEL_DIR := device/google/stallion-kernels/6.1/grapheneos
+TARGET_KERNEL_DIR ?= device/google/stallion-kernels/6.1/grapheneos
 
 include vendor/adevtool/config/mk/google_devices/platform/zumapro/device.mk
